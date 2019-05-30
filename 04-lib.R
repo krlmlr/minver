e936b5f6be1c2988f3e632c41c3815b39bd8fb83
Kirill Müller <krlmlr+r@mailbox.org>
2019-05-30 14:23:01 +0200

    Oops

--- 04-lib.R
+++ 04-lib.R
@@ -5,7 +5,7 @@ unlink(lib, recursive = TRUE, force = TRUE)
 dir.create(lib, showWarnings = FALSE)
 
 invisible(remotes::install_deps)
-withr::with_libpaths(lib, remotes::install_deps(pkg_path, dependencies = TRUE), action = "prepend")
+withr::with_libpaths(lib, remotes::install_deps(pkg_path, dependencies = TRUE), action = "prefix")
 
 # Some packages require memoise
 withr::with_libpaths(lib, install.packages("memoise"))
